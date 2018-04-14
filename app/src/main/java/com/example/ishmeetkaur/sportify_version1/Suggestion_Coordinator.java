048eab495626f67ec874644107742526dbdbf675
shubham15098 <shubham15098@iiitd.ac.in>
2018-04-15 02:44:50 +0530

    suggestion: delete functionality added

--- app/src/main/java/com/example/ishmeetkaur/sportify_version1/Suggestion_Coordinator.java
+++ app/src/main/java/com/example/ishmeetkaur/sportify_version1/Suggestion_Coordinator.java
@@ -10,12 +10,18 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,6 +34,8 @@ public class Suggestion_Coordinator extends AppCompatActivity
     FirebaseDatabase firebaseDatabase;
 
     DatabaseReference databaseReference;
+    Query firebase_query;
+
     private ArrayList<String> query_type1 = new ArrayList<>();
     private ArrayList<String> query_type2 = new ArrayList<>();
     private ArrayList<String> query_type3 = new ArrayList<>();
@@ -37,13 +45,18 @@ public class Suggestion_Coordinator extends AppCompatActivity
     private ExpandableListAdapter listAdapter;
     private List<String> listDataHeader;
     private HashMap<String,List<String>> listHash;
-
+    private HashMap<String,Integer> myHash;
+    private String CoordinatorId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestion__coordinator);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        CoordinatorId = firebaseUser.getUid();
 
         getFirebaseData();
 
@@ -52,9 +65,7 @@ public class Suggestion_Coordinator extends AppCompatActivity
 
     void getFirebaseData()
     {
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = mAuth.getCurrentUser();
-        final String CoordinatorId = firebaseUser.getUid();
+
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Suggestions").child(CoordinatorId);
 
@@ -81,6 +92,20 @@ public class Suggestion_Coordinator extends AppCompatActivity
                 {
                     query_type4.add(s_class.getText());
                 }
+
+//                if (!myHash.containsKey(s_class.getText())) {
+//                    myHash.put(s_class.getText(),s_class.getType());
+//                } else {
+//                    // the key already exists in the map. It might be a bug, or it might be
+//                    // a valid situation that you have to decide how to handle
+//                }
+
+
+
+                listView = (ExpandableListView)findViewById(R.id.lvExp);
+                initData();
+                listAdapter = new ExpandableListAdapter(getBaseContext(),listDataHeader,listHash);
+                listView.setAdapter(listAdapter);
             }
 
             @Override
@@ -90,8 +115,33 @@ public class Suggestion_Coordinator extends AppCompatActivity
             }
 
             @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            public void onChildRemoved(DataSnapshot dataSnapshot)
+            {
+                // clear the item from the list
+                Suggestion_class s_class2 = dataSnapshot.getValue(Suggestion_class.class);
+                int to_delete_from = s_class2.getType();
 
+                if(to_delete_from == 1)
+                {
+                    query_type1.remove(s_class2.getText());
+                }
+                else if(to_delete_from == 2)
+                {
+                    query_type2.remove(s_class2.getText());
+                }
+                else if(to_delete_from == 3)
+                {
+                    query_type3.remove(s_class2.getText());
+                }
+                else if(to_delete_from == 4)
+                {
+                    query_type4.remove(s_class2.getText());
+                }
+
+                listView = (ExpandableListView)findViewById(R.id.lvExp);
+                initData();
+                listAdapter = new ExpandableListAdapter(getBaseContext(),listDataHeader,listHash);
+                listView.setAdapter(listAdapter);
             }
 
             @Override
@@ -109,11 +159,9 @@ public class Suggestion_Coordinator extends AppCompatActivity
             public void onDataChange(DataSnapshot dataSnapshot)
             {
 
-                //after we finish adding all the child, this function will get called
-                listView = (ExpandableListView)findViewById(R.id.lvExp);
-                initData();
-                listAdapter = new ExpandableListAdapter(getApplicationContext(),listDataHeader,listHash);
-                listView.setAdapter(listAdapter);
+
+
+
 
             }
 
@@ -142,5 +190,40 @@ public class Suggestion_Coordinator extends AppCompatActivity
         listHash.put(listDataHeader.get(1),query_type2);
         listHash.put(listDataHeader.get(2),query_type3);
         listHash.put(listDataHeader.get(3),query_type4);
+    }
+
+    public void myClickHandler(View v)
+    {
+        Log.v("going","good bro");
+        LinearLayout vwParentRow = (LinearLayout)v.getParent();
+
+        TextView child = (TextView)vwParentRow.getChildAt(0);
+        String input = child.getText().toString();
+        Log.v("going",input);
+        deleteFromDatabase(input);
+    }
+
+    public void deleteFromDatabase(String input)
+    {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebase_query = firebaseDatabase.getReference("Suggestions").child(CoordinatorId).orderByChild("Text").equalTo(input);
+
+        firebase_query.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot appleSnapshot: dataSnapshot.getChildren())
+                {
+                    appleSnapshot.getRef().removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+            }
+        });
     }
 }
