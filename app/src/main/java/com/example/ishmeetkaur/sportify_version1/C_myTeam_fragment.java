f58f1072fef3b81b1af608a0a2b71f94ac274480
ishmeetkaur <ishmeet15042@iiitd.ac.in>
2018-04-15 06:15:29 +0530

    final push

--- app/src/main/java/com/example/ishmeetkaur/sportify_version1/C_myTeam_fragment.java
+++ app/src/main/java/com/example/ishmeetkaur/sportify_version1/C_myTeam_fragment.java
@@ -34,6 +34,7 @@ public class C_myTeam_fragment extends Fragment {
 
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_c_my_team_fragment, container, false);
+
         Button teamdetailsbutton = (Button) v.findViewById(R.id.buttonTeamDetails);
         teamdetailsbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +43,8 @@ public class C_myTeam_fragment extends Fragment {
                 startActivity(i);
             }
         });
+
+        //work here for attendance and suggestions.
 
         return v;
     }
